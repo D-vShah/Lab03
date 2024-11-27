1132eae61511f40ded77c681939fdaa1d706b052
Dev Shah <dshah416@gatech.edu>
2024-11-26 22:43:21 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import info
+
+# Title of App
+st.title("Web Development Lab03")
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 50, Web Development - Section A")
+st.subheader("Dev Shah, Trystan Davy")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. Dev's Portfolio: An online portfolio showcasing Dev Shah's educational and professional history.
+2. Trystan's Protfolio: An online portfolio exhibiting Trystan Davy's academic and professional prowess.
+3. Player Similarity Finder: Input your stats to find a footballer like you!
+4. Player Career Description: Learn about your favorite football player's career.
+
+""")
---
+++ info2.py
@@ -0,0 +1,73 @@
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "WebDevelopmentLab03/Images/MyPic.jpg"
+about_me = "I'm Trystan Davy, a student at the Georgia Institute of Technology pursuing a Bachelor's of Science in computer science."
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/trystan-davy-714626335/"
+my_github_url = "https://github.com/TDavy-tech/"
+my_email_address = "tdavycga@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2028',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CP 4190"], 
+    "names":["Intro to CS", "Intro Climate Change Plan"], 
+    "semester_taken":["1st", "1st"],
+    "skills":["Python is the best programming language", "Pandas is confusing"],
+    }
+experience_data = {
+    "Typist/Assistant Writer": (["- Wrote exemplary texts for 11th grade class",
+                                                                          "- Assisted students with typing and authoring argumentative and informational essays"],"WebDevelopmentLab03/Images/Write.jpg"),
+    "Video Game Developer/Tester ":(["- Developed prototype video games using a game engine",
+                                                           "- Playtested various games made by myself and colleagues"],"WebDevelopmentLab03/Images/Tester.png"),
+    "Carpenter’s Apprentice":(["- Designed and constructed appliances using wood and woodcutting machinery in senior year of high school "],"WebDevelopmentLab03/Images/R.jpg")
+
+}
+
+projects_data = {
+    "Streamlit Hero/Villain Analysis": "Created a Streamlit application which takes three traits from a user and determines how many heroes and villains have those traits, as well as what the user is most likely to be based on that info.",
+}
+
+programming_data = {
+    "Python": 90,
+    "Javascript": 20,
+    "HTML": 50,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Javascript": "☕",
+    "HTML": "🔍",
+}
+spoken_icons = {
+    "English": "",
+    "Spanish":""
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Able to read",
+}
+leadership_data = {
+    "Math Tutor at Towers High School": (["- Served as a tutor for precalculus class and calculus class, assisting 11th-12th grade students with mathematical subjects (Trigonometry, Matrices, Algebra, Limits)"],"WebDevelopmentLab03/Images/What.webp"),
+
+}
+activity_data={
+    "Volunteering": ["- Tech Assistant: Assisted 12th grade engineering teacher with pairing and operating unmanned aerial vehicles", 
+            "- Volunteer Writer/Artist: Created symbolic art projects and written works for 12th grade multicultural literature class"]
+}
